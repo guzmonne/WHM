53e8f99d10dad86bb8ae4189bb69bf276f894a62
unknown <GMonne@IT-ESP04.solit-servtec.conatel.com.uy>
2015-01-05 17:13:05 -0200

    added netsh info on table

---
+++ app/js/controllers/netshIndex.controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Netsh = require('./js/modules/netsh.js');
+
+App.controller('NetshIndexCtrl', function($scope, $interval){
+  var stop;
+
+  $scope.networks = {};
+
+  $scope.getReadings = function(){
+    Netsh.wifiNetworkScan(function(err, networks){
+      if (err) return console.log(err);
+      $scope.networks = networks;
+      $scope.$apply();
+    });
+  };
+
+  $scope.stopReadings = function(){
+    if (angular.isDefined(stop)) $interval.cancel(stop);
+    stop = undefined;
+  };
+
+  stop = $interval(function(){ $scope.getReadings(); }, 1000);
+
+  $scope.$on('$destroy', function(){ $scope.stopReadings(); });
+
+  $scope.getReadings();
+});
